bee9c5f2d81a659997272b4d8269bbe8aa18167d
NdFeB <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-02-14 20:43:10 +0100

    esbuild: drop console/debugger

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    drop: ["console", "debugger"],
+  },
   resolve: {
     alias: [
       {
